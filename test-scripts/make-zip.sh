c2da7d074cb5e0ddee9f9e6afe46783bf6806dc0
Natalia Ortiz <natorgom@gmail.com>
2021-07-20 17:16:52 +0200

    zip output3

--- test-scripts/make-zip.sh
+++ test-scripts/make-zip.sh
@@ -3,6 +3,7 @@ echo Creating zip file
 yum update -y
 yum install -y zip
 
-zip -r -j zip-output/src.zip spring-petclinic/src/*
+cd spring-getclinic/src
+zip -r ../../zip-output/src.zip .
 
 ls -la zip-output

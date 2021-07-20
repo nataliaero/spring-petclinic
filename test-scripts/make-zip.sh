7fb6a1c36a65d7b4ecc3abf64bc40fab6d18bd8e
Natalia Ortiz <natorgom@gmail.com>
2021-07-20 13:53:03 +0200

    zip

--- test-scripts/make-zip.sh
+++ test-scripts/make-zip.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/sh
 echo Creating zip file
 yum update -y
-#yum install -y zip
+yum install -y zip
 
-#cd zip -r -j src.zip spring-petclinic/src/*
+zip -r -j src.zip spring-petclinic/src/*
+
+ls -la .

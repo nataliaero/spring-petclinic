43ddb3161f67acb56840f0933439282b7fef7c1b
Natalia Ortiz <natorgom@gmail.com>
2021-07-20 16:53:21 +0200

    save zip in spring-petclinic/

--- test-scripts/make-zip.sh
+++ test-scripts/make-zip.sh
@@ -3,6 +3,6 @@ echo Creating zip file
 yum update -y
 yum install -y zip
 
-zip -r -j src.zip spring-petclinic/src/*
+zip -r -j spring-petclinic/src.zip spring-petclinic/src/*
 
 ls -la .
--- test-scripts/upload-zip.sh
+++ test-scripts/upload-zip.sh
@@ -3,14 +3,11 @@ echo Unzip file
 yum update -y
 yum install -y zip
 
-unzip zip-output/src.zip
+unzip spring-petclinic/src.zip
 
-ls -laR .
 echo "===== see what zip-out has: "
 ls -la zip-output
 echo "===== see what spring-petclinic has: "
 ls -la spring-petclinic
 
-echo "===== see what spring-petclinic/test-scripts has: "
-ls -la spring-petclinic/test-scripts
 

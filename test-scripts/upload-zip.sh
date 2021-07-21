f42a8cc604bbef23f63e49b5f00b1c819723cde4
Natalia Ortiz <natorgom@gmail.com>
2021-07-21 15:01:20 +0200

    add comment sessionToken

--- test-scripts/upload-zip.sh
+++ test-scripts/upload-zip.sh
@@ -3,9 +3,6 @@ yum update -y && yum install -y jq
 
 assume_deployment_role() {
   echo "Region: " $REGION
-  echo AWS_ACCESS_KEY $AWS_ACCESS_KEY
-  echo AWS_SECRET_ACCESS_KEY $AWS_SECRET_ACCESS_KEY
-  echo AWS_ACCOUNT_ID $AWS_ACCOUNT_ID
 
   REGION=$REGION
   export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
@@ -21,6 +18,8 @@ assume_deployment_role() {
   AWS_ACCESS_KEY_ID=$(echo "$CREDS_JSON" | jq -r ".Credentials.AccessKeyId")
   AWS_SECRET_ACCESS_KEY=$(echo "$CREDS_JSON" | jq -r ".Credentials.SecretAccessKey")
   AWS_SESSION_TOKEN=$(echo "$CREDS_JSON" | jq -r ".Credentials.SessionToken")
+
+  echo AWS_SESSION_TOKEN $AWS_SESSION_TOKEN
 
   export AWS_ACCESS_KEY_ID
   export AWS_SECRET_ACCESS_KEY

aa2796e02fe5090e01eef247ee501f0feb684886
Natalia Ortiz <natorgom@gmail.com>
2021-07-21 12:19:14 +0200

    fix variables names

--- test-scripts/upload-zip.sh
+++ test-scripts/upload-zip.sh
@@ -3,7 +3,7 @@ yum update -y && yum install -y jq
 
 assume_deployment_role() {
   REGION=$REGION
-  export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+  export AWS_ACCESS_KEY=$AWS_ACCESS_KEY
   export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
   unset AWS_SESSION_TOKEN
 
@@ -15,11 +15,11 @@ assume_deployment_role() {
       --role-session-name webgis-staging-admin-nat
   )
 
-  AWS_ACCESS_KEY_ID=$(echo "$CREDS_JSON" | jq -r ".Credentials.AccessKeyId")
+  AWS_ACCESS_KEY=$(echo "$CREDS_JSON" | jq -r ".Credentials.AccessKeyId")
   AWS_SECRET_ACCESS_KEY=$(echo "$CREDS_JSON" | jq -r ".Credentials.SecretAccessKey")
   AWS_SESSION_TOKEN=$(echo "$CREDS_JSON" | jq -r ".Credentials.SessionToken")
 
-  export AWS_ACCESS_KEY_ID
+  export AWS_ACCESS_KEY
   export AWS_SECRET_ACCESS_KEY
   export AWS_SESSION_TOKEN
 }

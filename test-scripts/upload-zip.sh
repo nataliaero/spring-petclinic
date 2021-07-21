969dc09bf976c578c992b04e867ba68abbaff0eb
Natalia Ortiz <natorgom@gmail.com>
2021-07-21 13:55:05 +0200

    use get-session-token to login

--- test-scripts/upload-zip.sh
+++ test-scripts/upload-zip.sh
@@ -8,23 +8,21 @@ assume_deployment_role() {
   echo AWS_ACCOUNT_ID $AWS_ACCOUNT_ID
 
   REGION=$REGION
-  export AWS_ACCESS_KEY=$AWS_ACCESS_KEY
+  export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
   export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
   unset AWS_SESSION_TOKEN
 
   CREDS_JSON=$(
     aws --region "$REGION" \
       --output json \
-      sts assume-role \
-      --role-arn arn:aws:iam::"$AWS_ACCOUNT_ID":role/role_admin \
-      --role-session-name webgis-staging-admin-nat
+      sts get-session-token
   )
 
-  AWS_ACCESS_KEY=$(echo "$CREDS_JSON" | jq -r ".Credentials.AccessKeyId")
+  AWS_ACCESS_KEY_ID=$(echo "$CREDS_JSON" | jq -r ".Credentials.AccessKeyId")
   AWS_SECRET_ACCESS_KEY=$(echo "$CREDS_JSON" | jq -r ".Credentials.SecretAccessKey")
   AWS_SESSION_TOKEN=$(echo "$CREDS_JSON" | jq -r ".Credentials.SessionToken")
 
-  export AWS_ACCESS_KEY
+  export AWS_ACCESS_KEY_ID
   export AWS_SECRET_ACCESS_KEY
   export AWS_SESSION_TOKEN
 }

f76df5713bfec8c5b703fdff9f2431acd7fcd372
Natalia Ortiz <natorgom@gmail.com>
2021-07-21 10:33:29 +0200

    remove yum from upload-zip

--- test-scripts/upload-zip.sh
+++ test-scripts/upload-zip.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/sh
 echo Unzip file
-yum update -y
-yum install -y zip
+# yum update -y
+# yum install -y zip
 
-unzip zip.image/src.zip
+# unzip zip.image/src.zip
 
-echo "===== see what zip-out has: "
-ls -la zip.image
-echo "===== see what spring-petclinic has: "
-ls -la spring-petclinic
+# echo "===== see what zip-out has: "
+# ls -la zip.image
+# echo "===== see what spring-petclinic has: "
+# ls -la spring-petclinic
 
 

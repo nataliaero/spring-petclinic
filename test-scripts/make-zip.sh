7bb174e3f9414460071cfcd8cbffef93accf5f83
Natalia Ortiz <natorgom@gmail.com>
2021-07-21 12:06:21 +0200

    add aws assume deployment role

--- test-scripts/make-zip.sh
+++ test-scripts/make-zip.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/sh
 echo "======== Creating zip file"
 yum update -y
 yum install -y zip
--- test-scripts/upload-zip.sh
+++ test-scripts/upload-zip.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/sh
-echo Unzip file
-# yum update -y
-# yum install -y zip
+yum update -y && yum install -y jq
 
-# unzip zip.image/src.zip
+assume_deployment_role() {
+  REGION=$REGION
+  export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+  export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+  unset AWS_SESSION_TOKEN
 
-echo "===== see what zip-out has: "
-ls -la zip-output
-# echo "===== see what spring-petclinic has: "
-# ls -la spring-petclinic
+  CREDS_JSON=$(
+    aws --region "$REGION" \
+      --output json \
+      sts assume-role \
+      --role-arn arn:aws:iam::"$AWS_ACCOUNT_ID":role/webgis-staging-admin-nat \
+      --role-session-name webgis-staging-admin-nat
+  )
+
+  AWS_ACCESS_KEY_ID=$(echo "$CREDS_JSON" | jq -r ".Credentials.AccessKeyId")
+  AWS_SECRET_ACCESS_KEY=$(echo "$CREDS_JSON" | jq -r ".Credentials.SecretAccessKey")
+  AWS_SESSION_TOKEN=$(echo "$CREDS_JSON" | jq -r ".Credentials.SessionToken")
+
+  export AWS_ACCESS_KEY_ID
+  export AWS_SECRET_ACCESS_KEY
+  export AWS_SESSION_TOKEN
+}
+
+
 
 
